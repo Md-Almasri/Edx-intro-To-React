08800cb54e2355aaaf237c92e1be0e6b977b413f
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-08-23 15:39:00 +0100

    create Welcome class component

--- module-2/createClassComponent.js
+++ module-2/createClassComponent.js
@@ -1,4 +1,10 @@
+class Welcome extends React.Component {
+	render() {
+		return <h3>Welcome to module 2</h3>
+	}
+}
+
 ReactDOM.render(
-	<h3>welcome to module 2</h3>,
+	<Welcome />,
 	document.getElementById("root")
 )
