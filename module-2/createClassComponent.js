be7f6982e77e54008b7722c22aa2472c1040a9a7
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-08-23 16:10:06 +0100

    add properties to class component

--- module-2/createClassComponent.js
+++ module-2/createClassComponent.js
@@ -1,10 +1,10 @@
 class Welcome extends React.Component {
 	render() {
-		return <h3>Welcome to module 2</h3>
+		return <h3>Welcome {this.props.message}</h3>
 	}
 }
 
 ReactDOM.render(
-	<Welcome />,
+	<Welcome message="module 2"/>,
 	document.getElementById("root")
 )
